7c7636013541844c8bac2bee30e4f4b1f6e1676d
harryzhou1987 <harry.xiao.zhou@gmail.com>
2022-05-08 17:10:44 +1200

    Use Parameter Store for Key Pair instead of uploading key pair file to github

--- h12-nullresource-provisioner.tf
+++ h12-nullresource-provisioner.tf
@@ -1,19 +1,19 @@
-resource "null_resource" "bastion_config" {
+# resource "null_resource" "bastion_config" {
 
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    private_key = file("private-key/${var.instance_keypair}.pem")
-    host     = module.ec2_public.public_ip
-  }
+#   connection {
+#     type     = "ssh"
+#     user     = "ec2-user"
+#     private_key = file("private-key/${var.instance_keypair}.pem")
+#     host     = module.ec2_public.public_ip
+#   }
 
-  provisioner "file" {
-    source      = "private-key/${var.instance_keypair}.pem"
-    destination = "/tmp/${var.instance_keypair}.pem"
-  }
+#   provisioner "file" {
+#     source      = "private-key/${var.instance_keypair}.pem"
+#     destination = "/tmp/${var.instance_keypair}.pem"
+#   }
 
-  provisioner "remote-exec" {
-    inline = ["sudo chmod 400 /tmp/${var.instance_keypair}.pem"]
-  }
+#   provisioner "remote-exec" {
+#     inline = ["sudo chmod 400 /tmp/${var.instance_keypair}.pem"]
+#   }
 
-}
+# }
